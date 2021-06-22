c1913e19c3541f429baf0804baf97ef895c8f89e
sammyreles22 <samuel.reles@gmail.com>
2021-06-21 21:34:05 -0400

    Added the RestClient class

---
+++ rest_client.py
@@ -0,0 +1,43 @@
+'''
+This file handles the entirety of the development of the REST calls to Matcha DB.
+
+sammatime22, 2021
+'''
+
+class RestClient():
+    '''
+    A REST Client for interfacing with Matcha DB. This allows the controller to ask for input args
+    independently to developing, conducting, and interpreting the result of a REST request against
+    the Matcha DB instance.
+    '''
+
+
+    def __init__(self):
+        '''
+        The constructor for the RestClient singleton.
+        '''
+
+    
+    def get_request():
+        '''
+        Runs a get request against the DB.
+        '''
+
+
+    def post_request():
+        '''
+        Runs a post request against the DB.
+        '''
+
+
+    def update_request():
+        '''
+        Runs an update (PUT) request against the DB.
+        '''
+
+
+    def delete_request():
+        '''
+        Runs a delete request against the DB.
+        '''
+
